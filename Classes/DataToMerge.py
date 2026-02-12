c4e549cd43b0b784bd5cd0dde7aaf001a5f82b6b
Alex <kazosalexandros@gmail.com>
2026-02-12 22:27:19 +0200

    feature/preprocess-service: fill preprocess_data.py and utils

---
+++ Classes/DataToMerge.py
@@ -0,0 +1,35 @@
+from pandas import DataFrame
+
+class DataToMerge:
+    """Container class for preprocessed datasets ready for merging.
+
+    Attributes
+    ----------
+    urls_uci : pd.DataFrame
+        UCI phishing dataset (already feature-rich).
+    urls_kaggle : pd.DataFrame
+        Kaggle phishing URLs with engineered URL-based features.
+    top_urls : pd.DataFrame
+        Top 1M websites dataframe (as loaded).
+    """
+
+    def __init__(
+            self,
+            urls_uci: DataFrame,
+            urls_kaggle: DataFrame,
+            top_urls: DataFrame,
+    ):
+        """Initialize DataToMerge with three preprocessed dataframes.
+
+        Parameters
+        ----------
+        urls_uci : pd.DataFrame
+            UCI phishing dataset.
+        urls_kaggle : pd.DataFrame
+            Kaggle phishing URLs with engineered features.
+        top_urls : pd.DataFrame
+            Top 1M websites dataframe.
+        """
+        self.urls_uci = urls_uci
+        self.urls_kaggle = urls_kaggle
+        self.top_urls = top_urls
